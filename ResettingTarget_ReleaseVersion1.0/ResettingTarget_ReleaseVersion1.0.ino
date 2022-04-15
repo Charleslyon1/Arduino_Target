c0b676895adfdc1ffa11abf668326320cba23eb5
Blady125 <72998653+Blady125@users.noreply.github.com>
2022-04-15 13:31:12 -0600

    **Final Code**

--- ResettingTarget_ReleaseVersion1.0/ResettingTarget_ReleaseVersion1.0.ino
+++ ResettingTarget_ReleaseVersion1.0/ResettingTarget_ReleaseVersion1.0.ino
@@ -54,6 +54,7 @@
  * Bluetooth Module: DSD Tech HM-18
  * Servo Motors (x4): MG 996R 
  * Sound Sensor: KY-038
+ * Force Sensitive Resistors: Thin Film Pressure Sensors
  * Targets: Any .22 Targets should work. Consult pictures.
  * 
  * 
@@ -69,9 +70,10 @@
  * firearms. If you wish to do this, please make sure you are doing it safely. 
  * If you want more resources on firearms, please reach out to the author (Blady125)
  */
+
 //======================================================================================================
 
-// Import needed libraries
+// Import required libraries
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
@@ -105,13 +107,13 @@ int state2 = LOW;
 int state3 = LOW;
 int state4 = LOW;
 
-// Booleans measuring if FSR is triggered 
+// Booleans measuring if FSR is triggered
 bool isDropped1;
 bool isDropped2;
 bool isDropped3;
 bool isDropped4;
 
-// Initialize Bluetooth adapter
+// Initialize Bluetooth module
 SoftwareSerial Bluetooth(7, 8);
 
 // Time and debounce used to stop FSR from triggering too quickly
@@ -119,13 +121,23 @@ long toggleTime = 0;
 long debounce = 40; 
 
 // Integer used to store incoming bluetooth values. Null for now
-int command;
-int previousCommand;
+int command = 0;
+int previousCommand = 0;
+
+// Values needed for the sound sensor
+int soundSensor = 2; 
+int shotCounter = 0;
+bool targetsDropped = false;
+
+// Values needed for the timer
+unsigned long startMillis;
+unsigned long endMillis;
+bool triggerTimer = false;
 
 
 //======================================================================================================
 
-
+// Initialize some variables, and set servos to default position
 void setup() {
   // Begin Serial communication at a baud rate of 9600:
   Serial.begin(9600);
@@ -144,26 +156,35 @@ void setup() {
    myServo2.write(0);
    myServo3.write(0);
    myServo4.write(120);
+
+   // Attach sound sensors to pin for operation
+   pinMode(soundSensor, INPUT);
 }
 
 
 
-
-
+// Main method loop
 void loop() {
-  command = Bluetooth.read();
-  // Manual reset from bluetooth
-  if(command == 49 && previousCommand != 49){
-    //Bluetooth input will force reset.
-    forceReset();
-  } 
-  // Automatic reset without bluetooth
-  else{
-    readFSRinputs();
-    checkTargets();
-    triggerTargetReset();
-  }
-  previousCommand = command;
+    // Manual reset from bluetooth
+    if(Bluetooth.available()) {
+      command = Bluetooth.read();
+      Serial.println(command);
+      if(command == 49){
+        //Bluetooth input will force reset.
+        forceReset();
+        Serial.println("Bluetooth Signal Received");
+      }
+    } 
+    // Automatic reset without bluetooth
+    else{
+      readFSRinputs();
+      checkTargets();
+      triggerTargetReset();
+     // Serial.println("Printing works");
+    }
+    previousCommand = command;
+    soundSensorRead();
+    scoreCalculation();
 }
 
 
@@ -181,6 +202,26 @@ void readFSRinputs(){
 
 
 
+//This method will read sound input, to measure gunshots
+void soundSensorRead(){
+  int sensorValue = digitalRead(soundSensor);
+  if(sensorValue == 1){
+    if(shotCounter == 1){
+      if(!triggerTimer){
+        startMillis = millis();
+        triggerTimer = true;  
+      }
+    }
+    delay(50);
+    shotCounter++;
+    Serial.print("Shots fired: ");
+    Serial.println(shotCounter);
+  }
+}
+
+
+
+
 // This method will check if the target has been knocked down
 void checkTargets(){
   /*
@@ -191,42 +232,29 @@ void checkTargets(){
    * rule out bounce, we then declare the target dropped and save the time. 
    */
 
-  // Checks values of the first Force Sensitive Resistor-------------------
+  // Checks values of the first Force Sensitive Resistor
   if (fsrReading1 > 200 && previous1 < 200 && millis() - toggleTime > debounce) {
     isDropped1 = true;
     toggleTime = millis();
   }
-  delay(debounce);
   previous1 = fsrReading1;
-
-
-  // Checks values of the second Force Sensitive Resistor------------------
+  // Checks values of the second Force Sensitive Resistor
   if (fsrReading2 > 200 && previous2 < 200 && millis() - toggleTime > debounce) {
     isDropped2 = true;
     toggleTime = millis();
   }
-
-  delay(debounce);
   previous2 = fsrReading2;
-
-
-  // Checks values of the third Force Sensitive Resistor-------------------
+  // Checks values of the third Force Sensitive Resistor
   if (fsrReading3 > 200 && previous3 < 200 && millis() - toggleTime > debounce) {
     isDropped3 = true;
     toggleTime = millis();
   }
-
-  delay(debounce);
   previous3 = fsrReading3;
-
-
-  // Checks values of the fourth Force Sensitive Resistor------------------
+  // Checks values of the fourth Force Sensitive Resistor
   if (fsrReading4 > 200 && previous4 < 200 && millis() - toggleTime > debounce) {
     isDropped4 = true;
     toggleTime = millis();
   }
-  //digitalWrite(ledpin4, state4);
-  delay(debounce);
   previous4 = fsrReading4;
 }
 
@@ -238,10 +266,13 @@ void triggerTargetReset(){
     // Check if all FSRs sense that the target has fallen
     if(isDropped1 && isDropped2 && isDropped3 && isDropped4){
 
+        // Stops Timer
+        endMillis = millis();
+      
         // Resets targets
         myServo1.write(120);
         delay (800);
-        myServo2.write(120);
+        myServo2.write(125);
         delay (800);
         myServo3.write(120);
         delay(800);
@@ -254,12 +285,15 @@ void triggerTargetReset(){
         myServo3.write(0);
         myServo4.write(120);
         delay(300);
-
+        
         // Resets target values
         isDropped1 = false;
         isDropped2 = false;
         isDropped3 = false;
         isDropped4 = false;
+
+        // Used for score calculation
+        targetsDropped = true;
     }
 }
 
@@ -271,7 +305,7 @@ void forceReset(){
     // Resets targets
     myServo1.write(120);
     delay (800);
-    myServo2.write(120);
+    myServo2.write(125);
     delay (800);
     myServo3.write(120);
     delay(800);
@@ -284,10 +318,40 @@ void forceReset(){
     myServo3.write(0);
     myServo4.write(120);
     delay(300);
-
+        
     // Resets target values
     isDropped1 = false;
     isDropped2 = false;
     isDropped3 = false;
     isDropped4 = false;
+}
+
+
+
+
+// Method that calculates score based on shots sensed
+void scoreCalculation(){
+    if(targetsDropped){
+      double math = (4.0/(double)shotCounter)*100.0;
+      Serial.print("Accuracy Score: ");
+      Serial.print(math);
+      Serial.println("%");
+      delay(100);
+      targetsDropped = false;
+      shotCounter = 0;
+      timerCalc();
+      triggerTimer = false;
+    }
+}
+
+
+
+
+// Method that handles a simple timer
+void timerCalc(){
+  double math = ((double)endMillis - (double)startMillis)/1000.00;
+  Serial.print("Time Taken: ");
+  Serial.print(math);
+  Serial.println(" seconds");
+  
 }
